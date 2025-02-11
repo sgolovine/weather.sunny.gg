d779dd36a9a259b83145ac1362079bc052a139ec
Sunny Golovine <sunny@glvn.co>
2025-02-11 12:27:36 -0700

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <p className="text-3xl font-bold text-center pt-4">
+        Hello from weather.sunny.gg
+      </p>
+    </>
+  );
+}
+
+export default App;
