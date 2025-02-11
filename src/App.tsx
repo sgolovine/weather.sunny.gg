4ddb1eb974abb37f05de91147090440213425c08
Sunny Golovine <sunny@glvn.co>
2025-02-11 12:54:17 -0700

    feat: start UI build

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,56 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const [geolocationState, setGeolocationState] = useState<{
+    loading: boolean;
+    error: boolean;
+  }>({
+    loading: true,
+    error: false,
+  });
+
+  const [coords, setCoords] = useState<{ lat: number; lng: number } | null>(
+    null
+  );
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (successCallback) => {
+        setGeolocationState({
+          loading: false,
+          error: false,
+        });
+        setCoords({
+          lat: successCallback.coords.latitude,
+          lng: successCallback.coords.longitude,
+        });
+      },
+      (errorCallback) => {
+        console.error(errorCallback);
+        setGeolocationState({
+          loading: false,
+          error: true,
+        });
+      }
+    );
+  }, []);
+
   return (
     <>
-      <p className="text-3xl font-bold text-center pt-4">
-        Hello from weather.sunny.gg
-      </p>
+      <div className="text-center pt-4 pb-2 border-b-2 border-gray-200">
+        <h1 className="text-xl font-bold">Weather</h1>
+        {geolocationState.loading && <p>Loading Location</p>}
+        {geolocationState.error && (
+          <p className="text-sm font-bold text-red-500">
+            An error occurred getting your current location
+          </p>
+        )}
+        {!!coords && (
+          <p className="text-sm">
+            {coords.lat}, {coords.lng}
+          </p>
+        )}
+      </div>
     </>
   );
 }
