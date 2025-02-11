73445b5560e3ac9fc51fe6629c1fd4747fa56829
Sunny Golovine <sunny@glvn.co>
2025-02-11 12:38:33 -0700

    chore: setup tooling

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import { QueryClientProvider } from "@tanstack/react-query";
+import queryClient from "./services/query-client.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>
 );
